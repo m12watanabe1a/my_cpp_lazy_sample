028c35ba1be9213bdf2abda4212910f16ad2f418
m12watanabe1a <40206149+m12watanabe1a@users.noreply.github.com>
2026-04-20 16:28:47 +0200

    add other type for test

--- tests/test_binary.cc
+++ tests/test_binary.cc
@@ -1,7 +1,7 @@
 #include "lazy/lazy.hh"
 #include <gtest/gtest.h>
 
-TEST(BinaryOperator, Add) {
+TEST(BinaryOperator, AddInt) {
   lazy::lazy<int> a(1);
   lazy::lazy<int> b(2);
   auto mid = a + b;
@@ -9,7 +9,15 @@ TEST(BinaryOperator, Add) {
   EXPECT_EQ(res, 3);
 }
 
-TEST(BinaryOperator, Subtract) {
+TEST(BinaryOperator, AddDouble) {
+  lazy::lazy<double> a(1.5);
+  lazy::lazy<double> b(2.5);
+  auto mid = a + b;
+  lazy::lazy<double> res = mid;
+  EXPECT_EQ(res, 4.0);
+}
+
+TEST(BinaryOperator, SubtractInt) {
   lazy::lazy<int> a(1);
   lazy::lazy<int> b(2);
   auto mid = a - b;
@@ -17,7 +25,15 @@ TEST(BinaryOperator, Subtract) {
   EXPECT_EQ(res, -1);
 }
 
-TEST(BinaryOperator, Multiply) {
+TEST(BinaryOperator, SubtractDouble) {
+  lazy::lazy<double> a(1.5);
+  lazy::lazy<double> b(2.5);
+  auto mid = a - b;
+  lazy::lazy<double> res = mid;
+  EXPECT_EQ(res, -1.0);
+}
+
+TEST(BinaryOperator, MultiplyInt) {
   lazy::lazy<int> a(2);
   lazy::lazy<int> b(3);
   auto mid = a * b;
@@ -25,10 +41,26 @@ TEST(BinaryOperator, Multiply) {
   EXPECT_EQ(res, 6);
 }
 
-TEST(BinaryOperator, Divide) {
+TEST(BinaryOperator, MultiplyDouble) {
+  lazy::lazy<double> a(1.5);
+  lazy::lazy<double> b(2.5);
+  auto mid = a * b;
+  lazy::lazy<double> res = mid;
+  EXPECT_EQ(res, 3.75);
+}
+
+TEST(BinaryOperator, DivideInt) {
   lazy::lazy<int> a(6);
   lazy::lazy<int> b(3);
   auto mid = a / b;
   lazy::lazy<int> res = mid;
   EXPECT_EQ(res, 2);
+}
+
+TEST(BinaryOperator, DivideDouble) {
+  lazy::lazy<double> a(6.0);
+  lazy::lazy<double> b(3.0);
+  auto mid = a / b;
+  lazy::lazy<double> res = mid;
+  EXPECT_EQ(res, 2.0);
 }
--- tests/test_unary.cc
+++ tests/test_unary.cc
@@ -1,16 +1,30 @@
 #include "lazy/lazy.hh"
 #include <gtest/gtest.h>
 
-TEST(UnaryOperator, Plus) {
+TEST(UnaryOperator, PlusInt) {
   lazy::lazy<int> a(1);
   auto mid = +a;
   lazy::lazy<int> res = mid;
   EXPECT_EQ(res, 1);
 }
 
-TEST(UnaryOperator, Minus) {
+TEST(UnaryOperator, PlusDouble) {
+  lazy::lazy<double> a(1.5);
+  auto mid = +a;
+  lazy::lazy<double> res = mid;
+  EXPECT_EQ(res, 1.5);
+}
+
+TEST(UnaryOperator, MinusInt) {
   lazy::lazy<int> a(1);
   auto mid = -a;
   lazy::lazy<int> res = mid;
   EXPECT_EQ(res, -1);
+}
+
+TEST(UnaryOperator, MinusDouble) {
+  lazy::lazy<double> a(1.5);
+  auto mid = -a;
+  lazy::lazy<double> res = mid;
+  EXPECT_EQ(res, -1.5);
 }
